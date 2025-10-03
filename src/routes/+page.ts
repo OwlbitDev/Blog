79e38592b7d1f69314c0c8a05fe136f225945398
hongui <honguilee@163.com>
2025-10-04 00:11:02 +0800

    Update route

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,4 @@
 import { redirect } from '@sveltejs/kit';
-export function load({ url }) {
-  // 如果直接访问根路径，进行重定向
-  if (url.pathname === '/') {
-    // 重定向到用户偏好语言或默认语言
-    throw redirect(302, `/en`);
-  }
-  
-  return {};
+export function load() {
+    redirect(302, `/en`);
 }
