1c8f9e1bedf1c691fbdb7c9aec8014d7042f9d23
hongui <honguilee@163.com>
2025-10-08 17:35:51 +0800

    Update coroutine overview

--- src/routes/[lang=lang]/note/+page.svelte
+++ src/routes/[lang=lang]/note/+page.svelte
@@ -1,8 +1,11 @@
 <script>
+	import Meta from '$lib/components/Meta.svelte'
 	import Note from '$lib/components/NoteItem.svelte'
 	const { data } = $props()
 
 </script>
+
+<Meta lang={data.lang} cat="note"/>
 
 <section class="py-20">
 	<div class="container mx-auto px-6">
